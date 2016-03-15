e70f172741df17474b65f4b6786291b6941e1f36
Shashik <shashik.banda@gmail.com>
2016-03-15 11:22:04 -0700

    added nicer layout scheme. retrieving and saving extra data from API call. removed footer because of problem where it wasn't sticking to the bottom of the page. will investiage later.

--- server/public/js/controllers/LegislatorPersonalPageController.js
+++ server/public/js/controllers/LegislatorPersonalPageController.js
@@ -6,20 +6,34 @@ app.controller('LegislatorPersonalPageController', function($scope,$routeParams,
 	.then(function(data){
 		$scope.name = data.data.legislator['@attributes'].firstlast;
 		$scope.party = data.data.legislator['@attributes'].party;
-		if($scope.party === 'D'){
-			$scope.party = "Democratic Party"
-		}
-		if($scope.party === 'R'){
-			$scope.party = "Republican Party"
-		}
+		// if($scope.party === 'D'){
+		// 	$scope.party = "Democratic Party"
+		// }
+		// if($scope.party === 'R'){
+		// 	$scope.party = "Republican Party"
+		// }
 		$scope.bioguideid = data.data.legislator['@attributes'].bioguide_id;
 		$scope.office = data.data.legislator['@attributes'].office;
 		$scope.picture = "https://theunitedstates.io/images/congress/225x275/"+data.data.legislator['@attributes'].bioguide_id+".jpg"
 
 		$http.get('/legislator/sunlight/'+$scope.bioguideid)
 		.then(function(moredata){
-			console.log(moredata);
-			console.log("asdfasdfasfd")
+			$scope.chamber = moredata.data.results[0].chamber
+			$scope.district = moredata.data.results[0].district;
+			$scope.staterank = moredata.data.results[0].state_rank;
+			$scope.termstart = moredata.data.results[0].term_start;
+			$scope.termend = moredata.data.results[0].term_end;
+			$scope.state = moredata.data.results[0].state;
+			if($scope.chamber === 'house'){
+				$scope.chamber = "Member of the House of Representatives"
+
+				$scope.house = true;
+			}
+			if($scope.chamber === 'senate'){
+				$scope.chamber = "Member of the United States Senate"
+				$scope.staterank = $scope.staterank
+				$scope.senate = true;
+			}
 		})
 	})
 })
