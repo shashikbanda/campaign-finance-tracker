6a7ef4f9f69143b7aaa4f4fc5775b4c845c2ba9a
Shashik <shashik.banda@gmail.com>
2016-03-15 13:00:00 -0700

    able to render chart.js pie chart onto one of the divs on congressmen personal profile page. need to import and add real data to the chart from open secret api.

--- server/public/js/controllers/LegislatorPersonalPageController.js
+++ server/public/js/controllers/LegislatorPersonalPageController.js
@@ -6,12 +6,6 @@ app.controller('LegislatorPersonalPageController', function($scope,$routeParams,
 	.then(function(data){
 		$scope.name = data.data.legislator['@attributes'].firstlast;
 		$scope.party = data.data.legislator['@attributes'].party;
-		// if($scope.party === 'D'){
-		// 	$scope.party = "Democratic Party"
-		// }
-		// if($scope.party === 'R'){
-		// 	$scope.party = "Republican Party"
-		// }
 		$scope.bioguideid = data.data.legislator['@attributes'].bioguide_id;
 		$scope.office = data.data.legislator['@attributes'].office;
 		$scope.picture = "https://theunitedstates.io/images/congress/225x275/"+data.data.legislator['@attributes'].bioguide_id+".jpg"
@@ -26,14 +20,36 @@ app.controller('LegislatorPersonalPageController', function($scope,$routeParams,
 			$scope.state = moredata.data.results[0].state;
 			if($scope.chamber === 'house'){
 				$scope.chamber = "Member of the House of Representatives"
-
 				$scope.house = true;
 			}
 			if($scope.chamber === 'senate'){
 				$scope.chamber = "Member of the United States Senate"
-				$scope.staterank = $scope.staterank
 				$scope.senate = true;
 			}
 		})
 	})
+	// Get the context of the canvas element we want to select
+	var data = [
+	    {
+	        value: 300,
+	        color:"#F7464A",
+	        highlight: "#FF5A5E",
+	        label: "Red"
+	    },
+	    {
+	        value: 50,
+	        color: "#46BFBD",
+	        highlight: "#5AD3D1",
+	        label: "Green"
+	    },
+	    {
+	        value: 100,
+	        color: "#FDB45C",
+	        highlight: "#FFC870",
+	        label: "Yellow"
+	    }
+	]
+	var ctx = document.getElementById("myChart").getContext("2d");
+
+	var myDoughnutChart = new Chart(ctx).Doughnut(data);
 })
